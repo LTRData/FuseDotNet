71d62cdee5cfea90e6eb544b0fe19085c8dd16af
LTRData <ol@ltr-data.se>
2024-05-30 00:17:23 +0200

    RentArray and ReturnArray

--- FuseDotNet/FuseMemory.cs
+++ FuseDotNet/FuseMemory.cs
@@ -56,6 +56,43 @@ public readonly struct FuseMemory<T>(nint pointer, int length) where T : unmanag
     public unsafe UnmanagedMemoryStream GetStream()
         => new((byte*)Address, Length * sizeof(T));
 
+    /// <summary>
+    /// Rents an array from shared array pool for use when arrays are needed. Note that
+    /// returned array could be larger than length of original native buffer, the Length
+    /// property of returned <see cref="ArraySegment{T}"/> indicates how much of the array
+    /// can be used for later copying back to native buffer.
+    /// 
+    /// After use, the returned array should be returned to the array pool again by calling
+    /// <see cref="ReturnArray" /> method. This will also copy array contents back to native
+    /// buffer so than Dokan driver receives the data written to array.
+    /// </summary>
+    /// <returns><see cref="ArraySegment{T}"/> containing a reference to rented array
+    /// and useful length of the array.</returns>
+    public ArraySegment<T> RentArray()
+    {
+        var array = ArrayPool<T>.Shared.Rent(Length);
+        Array.Clear(array, 0, Length);
+        return new(array, 0, Length);
+    }
+
+    /// <summary>
+    /// Returns an array to shared array pool that was previously returned by <see cref="RentArray"/>
+    /// and copies array contents to original native buffer so that Dokan driver receives the
+    /// data written to the array.
+    /// </summary>
+    /// <param name="array">Array returned by <see cref="RentArray"/></param>
+    /// <param name="clearArray">Indicates whether the contents of the buffer should be cleared before reuse.</param>
+    public void ReturnArray(T[] array, bool clearArray = false)
+    {
+        array.AsSpan(0, Length).CopyTo(Span);
+        ArrayPool<T>.Shared.Return(array, clearArray);
+    }
+
+    /// <summary>
+    /// Returns a string describing the native buffer. If element type is <see cref="char"/>,
+    /// a string with the same characters as in original buffer is returned.
+    /// </summary>
+    /// <returns>String describing the native buffer</returns>
     public override string ToString()
     {
         if (Address == 0)
@@ -131,6 +168,37 @@ public readonly struct ReadOnlyFuseMemory<T>(nint pointer, int length) where T :
     public unsafe UnmanagedMemoryStream GetStream()
         => new((byte*)Address, Length * sizeof(T), Length * sizeof(T), FileAccess.Read);
 
+    /// <summary>
+    /// Rents an array from shared array pool for use when arrays are needed. Data from
+    /// original native buffer is copied into the array. Note that returned array could
+    /// be larger than length of original native buffer, the Length property of returned
+    /// <see cref="ArraySegment{T}"/> indicates how much of the array contains valid data.
+    /// 
+    /// After use, the returned array should be returned to the array pool again by calling
+    /// <see cref="ReturnArray" /> method.
+    /// </summary>
+    /// <returns><see cref="ArraySegment{T}"/> containing a reference to rented array
+    /// and length of valid data in the array.</returns>
+    public ArraySegment<T> RentArray()
+    {
+        var array = ArrayPool<T>.Shared.Rent(Length);
+        Span.CopyTo(array);
+        return new(array, 0, Length);
+    }
+
+    /// <summary>
+    /// Returns an array to shared array pool that was previously returned by <see cref="RentArray"/>
+    /// </summary>
+    /// <param name="array">Array returned by <see cref="RentArray"/></param>
+    /// <param name="clearArray">Indicates whether the contents of the buffer should be cleared before reuse.</param>
+    public void ReturnArray(T[] array, bool clearArray = false)
+        => ArrayPool<T>.Shared.Return(array, clearArray);
+
+    /// <summary>
+    /// Returns a string describing the native buffer. If element type is <see cref="char"/>,
+    /// a string with the same characters as in original buffer is returned.
+    /// </summary>
+    /// <returns>String describing the native buffer</returns>
     public override string ToString()
     {
         if (Address == 0)
